048f041e6c4d7716ce2dd75b263dc653657c6bbb
gabrielle <gabriellelorot@gmail.com>
2022-03-04 17:54:37 +0100

    loader for ThinkTankList.jsx

--- src/components/thinktankList/ThinkTankList.jsx
+++ src/components/thinktankList/ThinkTankList.jsx
@@ -164,66 +164,81 @@ export default memo(function ThinkTankList({
   const [containerCount, setContainerCount] = useState(0);
   return (
     <div className="big_container">
-      {<Loader loading={loading} />}
+      {/*{<Loader loading={loading} />}*/}
       {/*{console.log("***********")}*/}
       {/*{console.log("allthinktanks", allThinkTanks)}*/}
-      {/*{console.log("bigarrayget", bigArray.get())}*/}
+      {/*{console.log("bigarrayget", bigArray.get().length)}*/}
       {bigArray.get().map((packOfTwelve, counter) => {
+        // console.log("packOfTwelve", packOfTwelve.secondProperty.length);
+        // }
+
         return (
-          <div
-            className={`thinktanklist__container container and${counter}`}
-            key={counter}
-          >
-            {packOfTwelve.secondProperty.map((p, i) => {
-              return i === packOfTwelve.secondProperty.length - 1 &&
-                !loading &&
-                !stopRequest ? (
-                <div
-                  key={`${p.id}-${i}`}
-                  ref={setLastElement}
-                  className={`areas area${(i % 12) + 1}`}
-                >
-                  <ThinkTankItem
-                    id={p.id}
-                    title={p.member.pseudo}
-                    brand={p.brand !== null ? p.brand.name : 0}
-                    url={p.brand !== null ? p.brand.link : 0}
-                    images={p.images}
-                    tags={p.tags}
-                    text={p.content}
-                    date={p.updated_at}
-                    showModal={showModal}
-                    setShowModal={setShowModal}
-                    modalVar={modalVar}
-                    setModalVar={setModalVar}
-                    onClick={openModal}
-                  />
-                </div>
-              ) : (
-                <div key={i} className={`areas area${(i % 12) + 1}`}>
-                  <ThinkTankItem
-                    id={p.id}
-                    title={p.member.pseudo}
-                    brand={p.brand !== null ? p.brand.name : 0}
-                    url={p.brand !== null ? p.brand.link : 0}
-                    images={p.images}
-                    tags={p.tags}
-                    text={p.content}
-                    date={p.updated_at}
-                    showModal={showModal}
-                    setShowModal={setShowModal}
-                    modalVar={modalVar}
-                    setModalVar={setModalVar}
-                    onClick={openModal}
-                  />
-                </div>
-              );
-            })}
-          </div>
+          packOfTwelve.secondProperty.length !== 0 && (
+            <div
+              className={`thinktanklist__container container and${counter}`}
+              key={counter}
+            >
+              {packOfTwelve.secondProperty.map((p, i) => {
+                return i === packOfTwelve.secondProperty.length - 1 &&
+                  !loading &&
+                  !stopRequest ? (
+                  <div
+                    key={`${p.id}-${i}`}
+                    ref={setLastElement}
+                    className={`areas area${(i % 12) + 1}`}
+                  >
+                    <ThinkTankItem
+                      id={p.id}
+                      title={p.member.pseudo}
+                      brand={p.brand !== null ? p.brand.name : 0}
+                      url={p.brand !== null ? p.brand.link : 0}
+                      images={p.images}
+                      tags={p.tags}
+                      text={p.content}
+                      date={p.updated_at}
+                      showModal={showModal}
+                      setShowModal={setShowModal}
+                      modalVar={modalVar}
+                      setModalVar={setModalVar}
+                      onClick={openModal}
+                    />
+                  </div>
+                ) : (
+                  <div key={i} className={`areas area${(i % 12) + 1}`}>
+                    <ThinkTankItem
+                      id={p.id}
+                      title={p.member.pseudo}
+                      brand={p.brand !== null ? p.brand.name : 0}
+                      url={p.brand !== null ? p.brand.link : 0}
+                      images={p.images}
+                      tags={p.tags}
+                      text={p.content}
+                      date={p.updated_at}
+                      showModal={showModal}
+                      setShowModal={setShowModal}
+                      modalVar={modalVar}
+                      setModalVar={setModalVar}
+                      onClick={openModal}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          )
         );
       })}
-      {loading && <p className="text-center">loading...</p>}
-      {stopRequest && <p className="text-center my-10">All by now !♥</p>}
+
+      {loading && (
+        <div className="scroll-loader">
+          <div className="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      )}
+      {stopRequest && <p className="text-center my-10">All by now !</p>}
       <Modal
         showModal={showModal}
         setShowModal={setShowModal}
